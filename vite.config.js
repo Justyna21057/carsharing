5e7e62f3a0ca0d57637e7f0c70bface33fa05db3
Justyna21057 <justyna21057@gmail.com>
2024-06-23 10:07:45 +0200

    added fonts

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "05_Dzien_3/03_Tworzenie_ukladu_strony/01_Jem_Warzywa";
+const exercisePath = "01_Car_Sharing";
 
 /**
  * Don't change those lines below
