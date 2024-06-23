ad9247eaebf70d25fe2ca956bf01d204dfc08474
Justyna21057 <justyna21057@gmail.com>
2024-06-23 09:38:55 +0200

    starting new project-copied files from old repository

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "05_Dzien_3/03_Tworzenie_ukladu_strony/01_Jem_Warzywa";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+  root: exercisePath,
+  server: {
+    port: 3000,
+  },
+});
